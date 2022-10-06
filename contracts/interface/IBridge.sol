87056f4c1fa5472e49adaf0793af1f2ecda9b5da
Fraccaroli Gianmarco <Fraccaman@users.noreply.github.com>
2022-10-06 11:55:18 +0200

    Testing bridge events (#40)
    * added trasferToNamada event test
    
    * feat: added transferToERC event testing
    
    * misc: update README.md
    
    Co-authored-by: Gianmarco Fraccaroli <>

--- contracts/interface/IBridge.sol
+++ contracts/interface/IBridge.sol
@@ -4,9 +4,9 @@ pragma solidity ^0.8.17;
 import "./ICommon.sol";
 
 interface IBridge is ICommon {
-    event TransferToNamada(uint256 indexed nonce, NamadaTransfer[] trasfers, uint256 confirmations);
+    event TransferToNamada(uint256 nonce, NamadaTransfer[] trasfers, uint256 confirmations);
 
-    event InvalidTransferToNamada(address indexed from, string indexed to, uint256 amount);
+    event InvalidTransferToNamada(address from, string to, uint256 amount);
 
     event TransferToERC(uint256 indexed nonce, ERC20Transfer[] transfers);
 
