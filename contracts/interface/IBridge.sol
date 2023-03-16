f9473101c4f083ce2113d2f5638fd93a1852fa4b
Tiago Carvalho <sugoiuguu@tfwno.gf>
2023-03-16 14:01:48 +0000

    Remove unused InvalidTransferToNamada event

--- contracts/interface/IBridge.sol
+++ contracts/interface/IBridge.sol
@@ -6,8 +6,6 @@ import "./ICommon.sol";
 interface IBridge is ICommon {
     event TransferToNamada(uint256 nonce, NamadaTransfer[] trasfers, bool[] validMap, uint256 confirmations);
 
-    event InvalidTransferToNamada(address from, string to, uint256 amount);
-
     event TransferToErc(uint256 indexed nonce, Erc20Transfer[] transfers, bool[] validMap, string relayerAddress);
 
     function authorize(
