91999f17ed2065ff7b78f38a96f1da7d3dfc8fae
Tiago Carvalho <sugoiuguu@tfwno.gf>
2023-07-17 19:24:26 +0100

    Mint wrapped NAM

--- contracts/contract/Token.sol
+++ contracts/contract/Token.sol
@@ -4,16 +4,9 @@ pragma solidity ^0.8.19;
 import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
 
 contract Token is ERC20 {
-    constructor(
-        string memory name,
-        string memory symbol,
-        uint256[] memory initialSupplies,
-        address[] memory addresses
-    ) ERC20(name, symbol) {
-        require(initialSupplies.length == addresses.length, "Invalid parameters.");
+    uint256 private constant MAX_UINT = 2 ** 256 - 1;
 
-        for (uint256 i = 0; i < addresses.length; ++i) {
-            _mint(addresses[i], initialSupplies[i]);
-        }
+    constructor(address _vaultAddress, string memory _name, string memory _symbol) ERC20(_name, _symbol) {
+        _mint(_vaultAddress, MAX_UINT);
     }
 }
