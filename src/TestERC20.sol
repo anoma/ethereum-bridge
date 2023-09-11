df9f657800eefcb8de1216acfd4ba3005d44cc2f
Tiago Carvalho <sugoiuguu@tfwno.gf>
2023-09-11 16:45:40 +0100

    Add TestERC20 contract

---
+++ src/TestERC20.sol
@@ -0,0 +1,14 @@
+//SPDX-License-Identifier: Apache-2.0
+pragma solidity ^0.8.17;
+
+import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
+import "@openzeppelin/contracts/access/Ownable.sol";
+
+/// This token can be easily made and used for testing transfers over the Ethereum bridge
+contract TestERC20 is ERC20, Ownable {
+    constructor() ERC20("TestERC20", "TE20") { }
+
+    function mint(address to, uint256 amount) public onlyOwner {
+        _mint(to, amount);
+    }
+}
