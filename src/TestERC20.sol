c1090aa69b27705ece00a2aa4d33c60815db0b03
Tiago Carvalho <sugoiuguu@tfwno.gf>
2023-09-13 13:16:50 +0100

    Change license and solidity version in TestERC20

--- src/TestERC20.sol
+++ src/TestERC20.sol
@@ -1,5 +1,5 @@
-//SPDX-License-Identifier: Apache-2.0
-pragma solidity ^0.8.17;
+//SPDX-License-Identifier: GPL-3.0
+pragma solidity ^0.8.21;
 
 import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
 import "@openzeppelin/contracts/access/Ownable.sol";
